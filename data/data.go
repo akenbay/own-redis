2417866648669d740275a92b609fdc8791bfa4b3
Aibar <akenbay@icloud.com>
2025-03-30 16:55:22 +0500

    Final 2

--- data/data.go
+++ data/data.go
@@ -9,7 +9,7 @@ var (
 	// Used regex statements
 	SetRe   = regexp.MustCompile(`^SET\s[\w]+\s[\w\s]+$`)
 	SetPXRe = regexp.MustCompile(`^SET\s[\w]+\s[\w\s]+\sPX\s\d+$`)
-	GetRe   = regexp.MustCompile(`^GET\s?[\w\s]*$`)
+	GetRe   = regexp.MustCompile(`^GET\s[\w\s]+$`)
 )
 
 // Sync library Map API used to avoid data races
