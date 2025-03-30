9967cc285ee6c30249010196f55ce3cdf2b75d00
Aibar <akenbay@icloud.com>
2025-03-30 14:20:11 +0500

    Feat: add starting udp server

--- main.go
+++ main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"flag"
 	"fmt"
+	"net"
 )
 
-var PortN int
+var PortNum int
 
 func main() {
-	flag.IntVar(&PortN, "port", 8080, "Port number")
+	flag.IntVar(&PortNum, "port", 8080, "Port number")
 	flag.Usage = func() {
 		fmt.Print("Own Redis\n\n",
 			"**Usage**\n",
@@ -18,4 +19,19 @@ func main() {
 			"- --port N\tPort number\n")
 	}
 	flag.Parse()
+
+	port := ":" + fmt.Sprint(PortNum)
+
+	addr, err := net.ResolveUDPAddr("udp", port)
+	if err != nil {
+		fmt.Println("Error resolving address:", err)
+		return
+	}
+
+	conn, err := net.ListenUDP("udp", addr)
+	if err != nil {
+		fmt.Println("Error creating UDP server:", err)
+		return
+	}
+	defer conn.Close()
 }
