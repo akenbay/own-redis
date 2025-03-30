4459d933856c51135d278ef2033b3552ecbe3f6d
Aibar <akenbay@icloud.com>
2025-03-30 15:13:54 +0500

    Feat: add data to store values and regexp to check requests

---
+++ data/data.go
@@ -0,0 +1,16 @@
+package data
+
+import (
+	"regexp"
+	"sync"
+)
+
+var (
+	// Used regex statements
+	SetRe   = regexp.MustCompile(`^SET[\w\s]*$`)
+	SetPXRe = regexp.MustCompile(`^SET [\w]+ [\w\s]+ PX \d+$`)
+	GetRe   = regexp.MustCompile(`^GET\s?[\w\s]*$`)
+)
+
+// Sync library Map API used to avoid data races
+var Data sync.Map
